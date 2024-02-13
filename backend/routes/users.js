03b48b75081bb3e228c2afce4cabc71778038d43
djordje34 <djordje00karisic@gmail.com>
2024-02-13 02:26:30 +0100

    path bug fix

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ObjectId } = require('mongodb');
 const crypto = require('crypto');
+const { authenticateUser } = require('./middleware/auth');
 
 const router = express.Router();
 
