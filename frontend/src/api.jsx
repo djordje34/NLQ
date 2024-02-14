f66bd28a2e8d9f6a0bb6f6fd6e48d52c671d7a89
djordje34 <djordje00karisic@gmail.com>
2024-02-14 02:23:20 +0100

    changed node root url

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001'; // Replace with your server URL
+const API_BASE_URL = 'http://localhost:3000'; //node root
 
 const api = axios.create({
   baseURL: API_BASE_URL,
