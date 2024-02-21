f8cfcd6dda58288a0f6c3d73d8e7bccab48b8f7b
djordje34 <djordje00karisic@gmail.com>
2024-02-21 02:28:55 +0100

    queries chat-like page

--- frontend/src/components/Queries.jsx
+++ frontend/src/components/Queries.jsx
@@ -1,19 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Button, Form } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+
+import api from '../api';
 
 const Queries = () => {
-  const { id } = useParams();
-  if (!id) {
-    return <div>Error: Nothing found not found</div>;
-  }
-  //fetch info using id
+  const { databaseId } = useParams();
+  const [queries, setQueries] = useState([]);
+  const [newQuery, setNewQuery] = useState('');
+  const [response, setResponse] = useState('');
+  
+  useEffect(() => {
+    const fetchQueries = async () => {
+      try {
+        const response = await api.get(`/queries/${databaseId}`, {
+          headers: {
+            Authorization: `${localStorage.getItem('token')}`,
+          },
+        });
+        console.log(response)
+        if (response.statusText === 'OK') {
+          const data = await response.data;
+          setQueries(data);
+        } else {
+          console.error('Error fetching queries (Status Error):', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching queries:', error.message);
+      }
+    };
+
+    fetchQueries();
+  }, [databaseId]);
+
+  const handleAddQuery = async () => { //u okviru nodejs srv treba da se pozove flask!
+    try {
+      const response = await api.post('/queries', { databaseId: databaseId, query: newQuery });
+      if (response.statusText === 'Created') {
+        await fetchQueries();
+        setNewQuery('');
+      } else {
+        console.error('Error adding query:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error adding query:', error.message);
+    }
+  };
 
   return (
-    <div className='dashboard' style={{color:'#f2f2f2'}}>
-      <h2>Database Details</h2>
-      <p>Database ID: {id}</p>
+    <div className='dashboard' style={{ color: '#f2f2f2', display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <div className='chat-section' style={{ flex: '1', overflowY: 'auto', width:'80%' }}>
+        {queries.map((query) => (
+          <div key={query._id} className='message'>
+            <div className='user-message'>
+              <p>Query: {query.query}</p>
+              <p>Created At: {new Date(query.createdAt).toLocaleString()}</p>
+            </div>
+            <div className='response-message'>
+              <p>Response: {query.response}</p>
+              <p>Created At: {new Date(query.createdAt).toLocaleString()}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className='input-section' data-bs-theme="dark" style={{ display: 'flex',
+       flexDirection: 'column',
+        width: '80%'}}>
+      <Form style={{ display: 'flex',
+        flexDirection: 'row',
+          position: 'relative',
+          border:'2px solid #2f2f2f',
+          borderRadius:'8px',
+          backgroundColor:'#212529'}}>
+      <Form.Control
+        as='textarea'
+        placeholder='Enter your query here'
+        value={newQuery}
+        onChange={(e) => setNewQuery(e.target.value)}
+        style={{ resize: 'none',
+         flex: '1',
+          width: '100%',
+          backgroundColor:'transparent',
+          border:'none'}}
+      />
+      <Button
+        variant='dark'
+        className='importBtn sendBtn'
+        onClick={handleAddQuery}
+        style={{}}>
+        <FontAwesomeIcon icon={faPaperPlane} />
+      </Button>
+    </Form>
+    </div>
+    <hr/>
     </div>
   );
-};
+  };
 
 export default Queries;
