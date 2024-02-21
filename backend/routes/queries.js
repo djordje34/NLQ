3ab5b0d9cb98c707798ed610b7fd0b219d1edc0b
djordje34 <djordje00karisic@gmail.com>
2024-02-21 01:28:31 +0100

    WIP queries new endpoints

--- backend/routes/queries.js
+++ backend/routes/queries.js
@@ -7,11 +7,11 @@ const router = express.Router();
 module.exports = (db) => {
   router.post('/', authenticateUser, async (req, res) => {
     try {
-      const { databaseId, query, response } = req.body;
+      const { databaseId, query } = req.body;
       const userId = req.userId;
 
-      if (!userId || !databaseId || !query || !response) {
-        return res.status(400).json({ error: 'userId, databaseId, query, and response are required' });
+      if (!userId || !databaseId || !query) {
+        return res.status(400).json({ error: 'userId, databaseId and query are required' });
       }
 
       const user = await db.collection('users').findOne({ _id: new ObjectId(userId) });
@@ -29,7 +29,8 @@ module.exports = (db) => {
       const newQuery = { 
         userId: user._id, 
         databaseId: database._id, 
-        query, response,
+        query, 
+        response,
         createdAt: new Date() };
       const result = await db.collection('queries').insertOne(newQuery);
 
@@ -40,23 +41,32 @@ module.exports = (db) => {
     }
   });
 
-  router.get('/', authenticateUser, async (req, res) => {
+  router.get('/:databaseId', authenticateUser, async (req, res) => { //proveri
     try {
       //const { userId } = req.params;
       const userId = req.userId;
+      const { databaseId: databaseId } = req.params;
       const user = await db.collection('users').findOne({ _id: new ObjectId(userId) });
 
       if (!user) {
         return res.status(404).json({ error: 'User not found' });
       }
 
-      const queries = await db.collection('queries').find({ userId: user._id }).toArray();
+      const database = await db.collection('databases').findOne({ _id: new ObjectId(requestDBId), userId: user._id });
+  
+      if (!database) {
+        return res.status(404).json({ error: 'Database not found' });
+      }
+
+      const queries = await db.collection('queries').find({ userId: user._id, databaseId:database._id }).toArray();
       res.json(queries);
     } catch (error) {
       console.error('Error fetching queries:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
+
+  //endpoint za samo 1 query delete!
 
   return router;
 };
