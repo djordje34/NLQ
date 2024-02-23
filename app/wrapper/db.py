384e13f262bea6c83c2de4066d2eba257628e0cd
djordje34 <djordje00karisic@gmail.com>
2024-02-23 16:34:53 +0100

    functions to manipulate filesys of project

--- app/wrapper/db.py
+++ app/wrapper/db.py
@@ -1,4 +1,4 @@
-from utility.utils import adopt_childfile
+from utility.utils import adopt_childfile, plot_er_diagram, check_file, create_path
 from langchain_community.utilities import SQLDatabase
 
 class Database:
@@ -11,11 +11,17 @@ class Database:
             filename (str): name of the database file (with extension)
         """
         filepath = adopt_childfile(user_id, filename)
+        self._name = filename
         self._path = adopt_childfile("..\data",filepath)
-        print(self._path)
-        self._db = SQLDatabase.from_uri(f"sqlite:///{self._path}")
+        self._erdpath = adopt_childfile("..\diagrams",filepath)
         
-    def get_schema(self,_:any):
+        self._path = f"sqlite:///{self._path}"
+        self._erdpath = f"{self._erdpath.rsplit('.',1)[0]}.png"
+        
+        self._db = SQLDatabase.from_uri(self._path)
+        self.plot()
+        
+    def get_schema(self,_:any)->any:
         """Get database schema from the wrapper object
 
         Args:
@@ -23,10 +29,28 @@ class Database:
                 returns last value read by the python interpreter
 
         Returns:
-            str: Database schema
+            any: Database schema
         """
         
         return self._db.get_table_info()
+    
+    def get_path(self)->str:
+        """Returns path to database file
+
+        Returns:
+            str: path to db file
+        """
+        
+        return self._path
+    
+    def get_erdpath(self)->str:
+        """Returns path to ER diagram plot file
+
+        Returns:
+            str: path to ER diagram plot file
+        """
+        
+        return self._erdpath
     
     def run_query(self, query:str):
         """Run a query in database from the wrapper object
@@ -39,3 +63,16 @@ class Database:
         """
         
         return self._db.run(query)
+    
+    def plot(self):
+        """Creates ER diagram for the given database
+        """
+        if check_file(self.get_erdpath()):
+            print(f"Using existing {self._name} ER diagram")
+            return
+        
+        create_path("\\".join(self.get_erdpath().split('\\')[:-1]))
+        print("\\".join(self.get_erdpath().split('\\')[:-1]))
+        plot_er_diagram(self._path, self._erdpath)
+        
+        
