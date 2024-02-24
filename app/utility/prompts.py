01d8a0b2e623c999328e0841900e47be678613ef
djordje34 <djordje00karisic@gmail.com>
2024-02-24 01:15:54 +0100

    updated prompts

--- app/utility/prompts.py
+++ app/utility/prompts.py
@@ -18,12 +18,12 @@ class Prompt(Enum):
     
     
     SQL_TO_NL_PROMPT = format_bwd_template(
-        """You are translating the provided SQL response into natural language, maintaining clarity and coherence.\n
+        """You are translating the provided SQL response into natural language, maintaining clarity, information and coherence.
         Translate the provided SQL response into clear and concise natural language. 
-        Craft a response based on the given table schema, question, SQL query, and SQL response, without reiterating the question or SQL output.\n
-        Based on the table schema below, question, sql query,
-        and sql response, 
-        write a natural language response:
+        Craft a response based on the given table schema, question, SQL query, and SQL response, without reiterating the question or SQL output.
+
+        Based on the table schema below, question, sql query, and sql response, write a natural language response:
+        Table Schema:\n
         {schema}
 
         Question: {question}
