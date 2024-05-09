cacb20c78024b1a706d1d9e09e6b4b42eda8a7ec
djordje34 <djordje00karisic@gmail.com>
2024-05-09 02:43:06 +0200

    Update __init__.py

--- app/wrapper/__init__.py
+++ app/wrapper/__init__.py
@@ -5,3 +5,4 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
+from langchain_community.llms import Ollama
