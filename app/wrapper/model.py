c19a91e1b1abd7acef4ba73bf762a651c6ed3c68
djordje34 <djordje00karisic@gmail.com>
2024-05-09 02:43:32 +0200

    replaced previous models with local Llama3

--- app/wrapper/model.py
+++ app/wrapper/model.py
@@ -17,12 +17,16 @@ class Model:
         REPLICATE_API_TOKEN = get_hf_key()
         self.replicate_id = "meta/llama-2-70b-chat:58d078176e02c219e11eb4da5a02a7830a283b14cf8f94537af893ccff5ee781"
         self.gemma_id = "google/gemma-7b-it"
-        
+        self.llm = Ollama(model="llama3")
+        self.db_generator = Ollama(model="llama3")
+        """
         self.llm = Replicate(model=self.replicate_id,
                               model_kwargs={"temperature": 0.01,
                                             "max_length": 1000,
                                             "top_p": 1})
-
+        """
+        """
         self.db_generator = HuggingFaceHub(repo_id=self.gemma_id,
                                            model_kwargs={"temperature":0.5,
                                                          "max_new_tokens": 2000})
+        """
